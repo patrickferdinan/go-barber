6f6b90fa2709b5535e6fa2cc77c6d991d6ad2248
patrickferdinan <patrick.ferdinan@hotmail.com>
2021-06-20 00:14:51 -0300

    ending connection to database

--- src/database/migrations/1623955061297-CreateAppointments.ts
+++ src/database/migrations/1623955061297-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1623955061297 implements MigrationInterfa
                         type: 'varchar',
                         isPrimary: true,
                         generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
                     },
                     {
                         name: 'provider',
