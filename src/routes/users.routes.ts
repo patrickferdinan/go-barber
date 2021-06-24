e43aff70548c968225223f19e8e1cea010c6668c
Patrick Ferdinan <patrick.ferdinan@hotmail.com>
2021-06-23 23:05:54 -0300

    record creation

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+
+import CreateUserService from '../services/CreateUserService';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    //Pegar os dados do usuario
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    // Deletar a informação do password de dentro do usuário, porém ela existe no banco.
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      update_at: user.update_at,
+    };
+
+    return response.json(userWithoutPassword);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default usersRouter;
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,44 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import User from '../models/User';
+
+// Inteface são criadas em classes e types em componentes.
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+ public async execute({name, email, password}: Request): Promise<User> {
+  // Criação do usuário
+  const usersRepository = getRepository(User);
+
+  // Verifica se ja existe o email criado
+  const checkUserExists = await usersRepository.findOne({
+    where: { email },
+  });
+
+  if (checkUserExists) {
+    throw new Error('Email address already used.');
+  }
+
+  // Criptografando o password
+  const hashedPassword = await hash(password, 8);
+
+  // Criando o usuário
+  const user = usersRepository.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  // Salvando o user no banco
+  await usersRepository.save(user);
+
+  return user;
+ }
+}
+
+export default CreateUserService;
