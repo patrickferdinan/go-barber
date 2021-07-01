35bf0bf477056b44d4d27b5db3257f07b9af4e97
Patrick Ferdinan <patrick.ferdinan@hotmail.com>
2021-07-01 00:01:39 -0300

    update avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,8 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -42,7 +44,26 @@ usersRouter.post('/', async (request, response) => {
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
   console.log(request.file)
 
-  return response.json({ok: true});
+  try {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id!,
+      avatarFilename: request.file?.filename!,
+    });
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      update_at: user.update_at,
+      avatarFilename: user.avatar,
+    };
+    return response.json(userWithoutPassword);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 export default usersRouter;
